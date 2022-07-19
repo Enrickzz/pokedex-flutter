fcf5b4bb32ec256be5bee2745c7735990d866683
Enrico Luis M. Cuison <61441278+Enrickzz@users.noreply.github.com>
2022-07-19 12:40:14 +0800

    Pokedex Model
    Was not saved before committing

--- lib/models/pokedex.dart
+++ lib/models/pokedex.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
 class Pokemon {
   String? name;
   String? url;
@@ -83,5 +87,53 @@ class Sub_type {
   Sub_type.fromJson(Map<String, dynamic> json) {
     name = json['name'].toString();
     url = json['url'].toString();
+  }
+}
+
+Color pokemonColor(String? firstT) {
+  switch (firstT) {
+    case "grass":
+      return Color.fromARGB(255, 63, 194, 85);
+    case "fire":
+      return Color.fromARGB(255, 228, 94, 41);
+    case "water":
+      return Color.fromARGB(255, 66, 132, 207);
+    case "bug":
+      return Color.fromARGB(255, 53, 88, 59);
+    case "normal":
+      return Color.fromARGB(255, 255, 255, 255);
+    case "electric":
+      return Color.fromARGB(255, 191, 218, 74);
+    case "poison":
+      return Color.fromARGB(255, 168, 63, 194);
+    case "ground":
+      return Color.fromARGB(255, 136, 100, 52);
+    case "fairy":
+      return Color.fromARGB(255, 208, 236, 142);
+    case "fighting":
+      return Color.fromARGB(255, 219, 151, 50);
+    case "psychic":
+      return Color.fromARGB(255, 196, 71, 144);
+    case "ghost":
+      return Color.fromARGB(255, 95, 110, 98);
+    case "rock":
+      return Color.fromARGB(255, 168, 170, 169);
+    case "ice":
+      return Color.fromARGB(255, 132, 225, 228);
+    case "dragon":
+      return Color.fromARGB(255, 235, 130, 44);
+    default:
+      return Colors.white;
+  }
+}
+
+Widget nullCheck(String? text) {
+  if (text == null) {
+    return const Text("");
+  } else {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+    );
   }
 }
