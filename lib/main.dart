d7a269c423f63580e906478d909c9ae8cabb29c0
Enrico Luis M. Cuison <61441278+Enrickzz@users.noreply.github.com>
2022-07-19 13:25:53 +0800

    Change Logic

--- lib/main.dart
+++ lib/main.dart
@@ -60,8 +60,22 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int offsetTracker = 0;
   ScrollController _scrollController = new ScrollController();
+
   @override
   initState() {
+    _scrollController.addListener(() {
+      if (_scrollController.position.atEdge) {
+        bool isTop = _scrollController.position.pixels == 0;
+        if (isTop) {
+        } else {
+          getPokemons(offsetTracker + 20).then((value) {
+            setState(() {
+              offsetTracker = offsetTracker + 20;
+            });
+          });
+        }
+      }
+    });
     getPokemons(offsetTracker).then((value) {
       setState(() {});
     });
@@ -70,169 +84,100 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
         // appBar: AppBar(
-        //   // Here we take the value from the MyHomePage object that was created by
-        //   // the App.build method, and use it to set our appbar title.
         //   title: Text(widget.title),
         // ),
         body: NotificationListener(
-            child: GridView.builder(
-              controller: _scrollController,
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 180,
-                  childAspectRatio: 1,
-                  crossAxisSpacing: 1,
-                  mainAxisSpacing: 1),
-              itemCount: pokemons.length,
-              itemBuilder: (context, index) {
-                //Data clean up/ null checking operations
-                String first = pokemons[index].name.toString();
-                String name = first[0].toUpperCase() + first.substring(1);
-                bool bool2ndType = true;
-                String? string2ndType;
-                // ignore: unnecessary_null_comparison
-                String? firstT = pokemons[index]
-                        .pokemonType
-                        ?.types
-                        .first
-                        .sub!
-                        .name
-                        .toString(),
-                    lastT = pokemons[index]
-                        .pokemonType
-                        ?.types
-                        .last
-                        .sub!
-                        .name
-                        .toString();
-                if (lastT != firstT) {
-                  string2ndType = pokemons[index]
-                      .pokemonType!
-                      .types[1]
-                      .sub!
-                      .name
-                      .toString();
-                }
-                Color boxColor = pokemonColor(firstT);
-                return Center(
-                  child: GestureDetector(
-                    child: Container(
-                      width: 165,
-                      height: 165,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: boxColor,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: Column(children: [
-                        SizedBox(height: 8),
-                        Text(
-                            style: TextStyle(color: Colors.black),
-                            textAlign: TextAlign.left,
-                            name + "\n"),
-                        Row(
-                          children: [
-                            Column(children: [
-                              Container(
-                                width: 70,
-                                // ignore: prefer_const_constructors
-                                child: nullCheck(pokemons[index]
-                                    .pokemonType
-                                    ?.types[0]
-                                    .sub!
-                                    .name
-                                    .toString()),
-                              ),
-                              SizedBox(height: 3),
-                              Visibility(
-                                  visible: bool2ndType,
-                                  child: Container(
-                                    width: 70,
-                                    // ignore: prefer_const_constructors
-                                    child: nullCheck(string2ndType),
-                                  ))
-                            ]),
-                            Container(
-                              width: 95,
-                              height: 100,
-                              alignment: Alignment.centerRight,
-                              child: Image.network(pokemons[index].img),
-                            )
-                          ],
-                        ),
-                      ]),
+      child: GridView.builder(
+        controller: _scrollController,
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 180,
+            childAspectRatio: 1,
+            crossAxisSpacing: 1,
+            mainAxisSpacing: 1),
+        itemCount: pokemons.length,
+        itemBuilder: (context, index) {
+          //Data clean up/ null checking operations
+          String first = pokemons[index].name.toString();
+          String name = first[0].toUpperCase() + first.substring(1);
+          bool bool2ndType = true;
+          String? string2ndType;
+          // ignore: unnecessary_null_comparison
+          String? firstT =
+                  pokemons[index].pokemonType?.types.first.sub!.name.toString(),
+              lastT =
+                  pokemons[index].pokemonType?.types.last.sub!.name.toString();
+          if (lastT != firstT) {
+            string2ndType =
+                pokemons[index].pokemonType!.types[1].sub!.name.toString();
+          }
+          Color boxColor = pokemonColor(firstT);
+          return Center(
+            child: GestureDetector(
+              child: Container(
+                width: 165,
+                height: 165,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: boxColor,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
                     ),
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => PokemonDetailed(
-                                  thisPokemon: pokemons[index])));
-                    },
+                  ],
+                ),
+                child: Column(children: [
+                  SizedBox(height: 8),
+                  Text(
+                      style: TextStyle(color: Colors.black),
+                      textAlign: TextAlign.left,
+                      name + "\n"),
+                  Row(
+                    children: [
+                      Column(children: [
+                        Container(
+                          width: 70,
+                          child: nullCheck(pokemons[index]
+                              .pokemonType
+                              ?.types[0]
+                              .sub!
+                              .name
+                              .toString()),
+                        ),
+                        SizedBox(height: 3),
+                        Visibility(
+                            visible: bool2ndType,
+                            child: Container(
+                              width: 70,
+                              child: nullCheck(string2ndType),
+                            ))
+                      ]),
+                      Container(
+                        width: 95,
+                        height: 100,
+                        alignment: Alignment.centerRight,
+                        child: Image.network(pokemons[index].img),
+                      )
+                    ],
                   ),
-                );
+                ]),
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            PokemonDetailed(thisPokemon: pokemons[index])));
               },
             ),
-            onNotification: (end) {
-              if (end is ScrollEndNotification) {
-                getPokemons(offsetTracker + 20).then((value) {
-                  offsetTracker = offsetTracker + 20;
-                  setState(() {});
-                });
-              }
-              return true;
-            }));
-  }
-}
-
-Color pokemonColor(String? firstT) {
-  switch (firstT) {
-    case "grass":
-      return Color.fromARGB(255, 63, 194, 85);
-    case "fire":
-      return Color.fromARGB(255, 228, 94, 41);
-    case "water":
-      return Color.fromARGB(255, 66, 132, 207);
-    case "bug":
-      return Color.fromARGB(255, 53, 88, 59);
-    case "normal":
-      return Color.fromARGB(255, 255, 255, 255);
-    case "electric":
-      return Color.fromARGB(255, 191, 218, 74);
-    case "poison":
-      return Color.fromARGB(255, 168, 63, 194);
-    case "ground":
-      return Color.fromARGB(255, 136, 100, 52);
-    case "fairy":
-      return Color.fromARGB(255, 208, 236, 142);
-    case "fighting":
-      return Color.fromARGB(255, 219, 151, 50);
-    case "psychic":
-      return Color.fromARGB(255, 196, 71, 144);
-    case "ghost":
-      return Color.fromARGB(255, 95, 110, 98);
-    case "rock":
-      return Color.fromARGB(255, 168, 170, 169);
-    case "ice":
-      return Color.fromARGB(255, 132, 225, 228);
-    case "dragon":
-      return Color.fromARGB(255, 235, 130, 44);
-    default:
-      return Colors.white;
+          );
+        },
+      ),
+    ));
   }
 }
 
@@ -247,11 +192,15 @@ Future<List<Pokemon>> getPokemons(int offset) async {
     test = Root.fromJson(jsonDecode(value.body));
     for (var i = 0; i < test.results.length; i++) {
       int query = i + offset + 1;
+      if (query > 905) {
+        break;
+      }
       pokemons.add(test.results[i]);
       pokemons[i + offset].id = query.toString(); //for onClick queries
       pokemons[i + offset].img =
           "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$query.png";
-      var typeResponse = http
+      print(pokemons[i + offset].img.toString() + "  <<<<<");
+      var typeResponse = await http
           .get(Uri.parse("https://pokeapi.co/api/v2/pokemon/$query"))
           .then((value) {
         pokemons[i + offset].pokemonType =
@@ -260,15 +209,4 @@ Future<List<Pokemon>> getPokemons(int offset) async {
     }
   });
   return test.results;
-}
-
-Widget nullCheck(String? text) {
-  if (text == null) {
-    return const Text("");
-  } else {
-    return Text(
-      text,
-      textAlign: TextAlign.center,
-    );
-  }
 }
